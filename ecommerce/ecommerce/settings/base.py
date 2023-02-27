60b898356fb61ef2db019bccb1191bee11567438
Joao Pedro Moura <jpmourabraga@gmail.com>
2023-02-26 21:00:39 -0300

    Updated pip, created .env and changed password location.

--- ecommerce/ecommerce/settings/base.py
+++ ecommerce/ecommerce/settings/base.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i1o3a&md-8(#fvt5npahklv)tf4_vir&b7lewswv=w%ha@u24p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
