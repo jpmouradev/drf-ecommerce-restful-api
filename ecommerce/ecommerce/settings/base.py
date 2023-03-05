dfcb9135882e086984fd850195400752c1154930
Joao Pedro Moura <jpmourabraga@gmail.com>
2023-03-05 14:51:59 -0300

    API Doc with Spectacular Schema.

--- ecommerce/ecommerce/settings/base.py
+++ ecommerce/ecommerce/settings/base.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # External
     'rest_framework',
+    'drf_spectacular',
     # Internal Apps
     'ecommerce.product',
 ]
@@ -120,4 +121,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+}
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from ecommerce.product import views
 
 router = DefaultRouter()
@@ -24,5 +24,7 @@ router.register(r"category", views.CategoryViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls))
+    path('api/', include(router.urls)),
+    path('api/schema/', SpectacularAPIView.as_view(), name="schema"),
+    path('api/schema/docs/', SpectacularSwaggerView.as_view(url_name="schema")),
 ]
