fbe48b31e1c2ba3a3df93ec990bf55c108b99b1c
Joao Pedro Moura <jpmourabraga@gmail.com>
2023-03-06 23:47:38 -0300

    Implemented Product.

--- ecommerce/ecommerce/product/serializers.py
+++ ecommerce/ecommerce/product/serializers.py
@@ -6,7 +6,7 @@ from .models import Brand, Category, Product
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = "__all__"
+        fields = ['name']
 
 
 class BrandSerializer(serializers.ModelSerializer):
@@ -16,6 +16,9 @@ class BrandSerializer(serializers.ModelSerializer):
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    brand = BrandSerializer()
+    category = CategorySerializer()
+
     class Meta:
         model = Product
         fields = "__all__"
--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -22,6 +22,7 @@ from ecommerce.product import views
 router = DefaultRouter()
 router.register(r"category", views.CategoryViewSet)
 router.register(r"brand", views.BrandViewSet)
+router.register(r"product", views.ProductViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
