ecccadf0553ee329dc08a3a4c9b777c004018ed7
Joao Pedro Moura <jpmourabraga@gmail.com>
2023-03-10 22:54:18 -0300

    A lot changed.

---
+++ drfecommerce/drfecommerce/drfecommerce/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+from rest_framework.routers import DefaultRouter
+
+from drfecommerce.product import views
+
+router = DefaultRouter()
+router.register(r"category", views.CategoryViewSet)
+router.register(r"brand", views.BrandViewSet)
+router.register(r"product", views.ProductViewSet)
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include(router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/docs/", SpectacularSwaggerView.as_view(url_name="schema")),
+]
--- ecommerce/ecommerce/product/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Brand, Category, Product
-
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Brand)
