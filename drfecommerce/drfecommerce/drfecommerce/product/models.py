63d67c6f7000786e3ab97ba02ab4f644c2f28488
Joao Pedro Moura <jpmourabraga@gmail.com>
2023-03-29 21:14:25 -0300

    Model clean method and testings.

--- drfecommerce/drfecommerce/drfecommerce/product/models.py
+++ drfecommerce/drfecommerce/drfecommerce/product/models.py
@@ -1,10 +1,21 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
+
+from .fields import OrderField
+
+
+class ActiveQueryset(models.QuerySet):
+    def isactive(self):
+        return self.filter(is_active=True)
 
 
 class Category(MPTTModel):
     name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=255)
+    is_active = models.BooleanField(default=False)
     parent = TreeForeignKey("self", on_delete=models.PROTECT, null=True, blank=True)
+    objects = ActiveQueryset.as_manager()
 
     class MPTTMeta:
         order_insertion_by = ["name"]
@@ -15,6 +26,8 @@ class Category(MPTTModel):
 
 class Brand(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    is_active = models.BooleanField(default=False)
+    objects = ActiveQueryset.as_manager()
 
     def __str__(self):
         return self.name
@@ -30,17 +43,32 @@ class Product(models.Model):
         "Category", on_delete=models.SET_NULL, null=True, blank=True
     )
     is_active = models.BooleanField(default=False)
+    objects = ActiveQueryset.as_manager()
 
     def __str__(self):
         return self.name
+
 
 class ProductLine(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=5)
     sku = models.CharField(max_length=100)
     stock_qty = models.IntegerField()
     product = models.ForeignKey(
-        Product,
-        on_delete=models.CASCADE,
-        related_name="product_line"
+        Product, on_delete=models.CASCADE, related_name="product_line"
     )
     is_active = models.BooleanField(default=False)
+    order = OrderField(unique_for_field="product", blank=True)
+    objects = ActiveQueryset.as_manager()
+
+    def clean(self):
+        qs = ProductLine.objects.filter(product=self.product)
+        for obj in qs:
+            if self.id != obj.id and self.order == obj.order:
+                raise ValidationError("Duplicate value.")
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super(ProductLine, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return str(self.sku)
--- drfecommerce/drfecommerce/drfecommerce/product/views.py
+++ drfecommerce/drfecommerce/drfecommerce/product/views.py
@@ -1,7 +1,12 @@
+from django.db import connection
 from drf_spectacular.utils import extend_schema
+from pygments import highlight
+from pygments.formatters import TerminalFormatter
+from pygments.lexers import SqlLexer
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from sqlparse import format
 
 from .models import Brand, Category, Product
 from .serializers import BrandSerializer, CategorySerializer, ProductSerializer
@@ -38,12 +43,24 @@ class ProductViewSet(viewsets.ViewSet):
     A simple Viewset for viewing all products
     """
 
-    queryset = Product.objects.all()
+    queryset = Product.objects.all().isactive()
+
     lookup_field = "slug"
 
     def retrieve(self, request, slug=None):
-        serializer = ProductSerializer(self.queryset.filter(slug=slug), many=True)
-        return Response(serializer.data)
+        serializer = ProductSerializer(
+            self.queryset.filter(slug=slug).select_related("category", "brand"),
+            many=True,
+        )
+        data = Response(serializer.data)
+
+        q = list(connection.queries)
+        print(len(q))
+        for qs in q:
+            sqlformatted = format(str(qs["sql"]), reindent=True)
+            print(highlight(sqlformatted, SqlLexer(), TerminalFormatter()))
+
+        return data
 
     @extend_schema(responses=ProductSerializer)
     def list(self, request):
@@ -52,12 +69,14 @@ class ProductViewSet(viewsets.ViewSet):
 
     @action(
         methods=["get"],
-        detail=True,
-        url_path=r"category/(?P<category>\w+)/all",
+        detail=False,
+        url_path=r"category/(?P<slug>[\w-]+)",
+    )
+    def list_product_by_category_slug(self, request, slug=None):
+        """
+        An endpoint to return products by category
+        """
+        serializer = ProductSerializer(
+            self.queryset.filter(category__slug=slug), many=True
         )
-    def list_product_by_category(self, request, category=None):
-        """
-        A n endpoint to return products by category
-        """
-        serializer = ProductSerializer(self.queryset.filter(category__name=category), many=True)
         return Response(serializer.data)
